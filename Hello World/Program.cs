98ba50d352449a2373fc66b0d2435f13e1e40db2
Tommy <tommyphelps16@gmail.com>
2020-10-05 11:16:04 -0500

    Added Title, and Text Color. Program now asks for name and you input it.

---
+++ Hello World/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hello_World
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hello World Program";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Hello what is your name?");
+            var name = Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
