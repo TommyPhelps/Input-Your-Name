912ab7762ebc6541e44fb5066313dffce260415f
Tommy <tommyphelps16@gmail.com>
2020-10-05 11:25:47 -0500

    Made Main method public.

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -2,9 +2,9 @@
 
 namespace Hello_World
 {
-    internal static class Program
+    class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.Title = "Hello World Program";
             Console.ForegroundColor = ConsoleColor.Green;
